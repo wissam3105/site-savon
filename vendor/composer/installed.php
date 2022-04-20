b2e71acaf5bfa98cb1d500aea120f7e100870f01
wissam3105 <wissamboufatah3105@gmail.com>
2022-04-20 12:38:34 +0200

    Added my project

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '00ea4f6994ddd7afd8de0185b4a89a31436903b0',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '00ea4f6994ddd7afd8de0185b4a89a31436903b0',
             'dev_requirement' => false,
         ),
         'symfony/password-hasher' => array(
